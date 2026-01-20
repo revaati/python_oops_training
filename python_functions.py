50ef248b0fc974f34495bf09657fc070c9d95d06
Revati Nimbalkar <Revati.Nimbalkar@bmwtechworks.in>
2026-01-20 15:20:07 +0530

    changes made in file

--- python_functions.py
+++ python_functions.py
@@ -6,3 +6,15 @@ y = int(input())
 
 result = add(x,y)
 print(result)
+
+
+def car(name):
+    if name == '':
+        print("You didn't enter the car name!")
+    else:
+        print("Its a")
+        for letter in name:
+            print(letter)
+
+name = input("Car name: ")
+car(name)
